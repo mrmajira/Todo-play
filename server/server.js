40b6fe88a8d2565f6d1acdf3f8eb0e57daec64ae
literally NO ONE <thisisaFakemake@stfu.com>
2020-07-06 05:32:34 +0330

    changed port order

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const   {mongoose}=     require("./db/mongoose"),
 
 
 let app=express();
-const port = 3000 || process.env.PORT ;
+const port = process.env.PORT||3000  ;
 
 app.use(bodyParser.json());
 
